8736e4f39cd51f8fc96f7ef5872c99b846199526
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-11 02:45:04 +0300

    Header is in progress now

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,24 @@
 import {Link, Outlet} from "react-router-dom";
+import { Menu } from 'antd';
+import {headerItems} from "../../helpers/headerItems";
+import {useState} from "react";
 
 export const Header = () => {
+    const [current, setCurrent] = useState('mail');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
     return (
-      <>
-        <header>
-            <h1>Header</h1>
-        </header>
-        <main>
-            <div>
-                <Outlet/>
-            </div>
-        </main>
-      </>
+        <>
+            <header>
+                <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={headerItems} />
+            </header>
+            <main>
+                <div>
+                    <Outlet/>
+                </div>
+            </main>
+        </>
     );
 }
