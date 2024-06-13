42adbb889c4f51d0a5ecb7ce45af5af0d4313339
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-14 01:16:53 +0300

    FlatSaleLinks component was finished

--- src/components/flatSaleLinks/FlatSaleLinks.jsx
+++ src/components/flatSaleLinks/FlatSaleLinks.jsx
@@ -4,27 +4,28 @@ import {Link} from "react-router-dom";
 export const FlatSaleLinks = () => {
     return (
       <>
-          <div>
+          <div className="flat__links-container">
+              <h2>Продажа</h2>
               <div>
-                  <Link to={''}>1-комнатные квартиры</Link>
+                  <Link className="flat__link" to={''}>1-комнатные квартиры</Link>
               </div>
               <div>
-                  <Link to={''}>2-комнатные квартиры</Link>
+                  <Link className="flat__link" to={''}>2-комнатные квартиры</Link>
               </div>
               <div>
-                  <Link to={''}>3-комнатные квартиры</Link>
+                  <Link className="flat__link" to={''}>3-комнатные квартиры</Link>
               </div>
               <div>
-                  <Link to={''}>4-комнатные квартиры</Link>
+                  <Link className="flat__link" to={''}>4-комнатные квартиры</Link>
               </div>
               <div>
-                  <Link to={''}>5-комнатные квартиры</Link>
+                  <Link className="flat__link" to={''}>5-комнатные квартиры</Link>
               </div>
               <div>
-                  <Link to={''}>6-комнатные квартиры</Link>
+                  <Link className="flat__link" to={''}>6-комнатные квартиры</Link>
               </div>
               <div>
-                  <Link to={''}>Больше комнат</Link>
+                  <Link className="flat__link" to={''}>Больше комнат</Link>
               </div>
           </div>
       </>
