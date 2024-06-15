2b686c551599368dad4db46d6d4c19acebd5a126
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-16 02:33:00 +0300

    MediumProperties component was made

--- src/pages/mediumProperties/MediumProperties.jsx
+++ src/pages/mediumProperties/MediumProperties.jsx
@@ -1,9 +1,32 @@
 import './MediumProperties.scss';
+import {useGetMediumPropertiesQuery} from "../../slices/propertiesPriceApi";
+import {Link} from "react-router-dom";
+import {PropertyCard} from "../../components/propertyCard/PropertyCard";
 
 export const MediumProperties = () => {
+    const { data: properties, error, isLoading } = useGetMediumPropertiesQuery();
     return (
-      <>
-        <h1>Medium</h1>
-      </>
+        <>
+            <div className="all-properties">
+                {isLoading && <div className="all-properties__loading">Loading...</div>}
+                <div className="all-properties__list">
+                    {properties && properties.data?.map(property => (
+                        <Link to={`/property/${property.id}`} className="all-properties__link" key={property?.id}>
+                            <PropertyCard
+                                image={property?.property_images[0]?.image_path}
+                                title={property?.title}
+                                price={property?.price}
+                                rooms={property?.rooms}
+                                area={property?.area}
+                                floor={property?.floor}
+                                city={property?.city}
+                                address={property?.address}
+                                description={property?.description}
+                            />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
