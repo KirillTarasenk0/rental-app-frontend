a7a2ad098e75a0b036692e2512d918686ab8a601
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-16 00:53:54 +0300

    Houses component was made

--- src/pages/houses/Houses.jsx
+++ src/pages/houses/Houses.jsx
@@ -1,9 +1,32 @@
 import './Houses.scss';
+import {useGetHousesPropertiesQuery} from "../../slices/propertiesTypeApi";
+import {Link} from "react-router-dom";
+import {PropertyCard} from "../../components/propertyCard/PropertyCard";
 
 export const Houses = () => {
+    const { data: properties, error, isLoading } = useGetHousesPropertiesQuery();
     return (
-      <>
-        <h1>Homes</h1>
-      </>
+        <>
+            <div className="all-properties">
+                {isLoading && <div className="all-properties__loading">Loading...</div>}
+                <div className="all-properties__list">
+                    {properties && properties.data?.map(property => (
+                        <Link to={`/property/${property.id}`} className="all-properties__link" key={property?.id}>
+                            <PropertyCard
+                                image={property?.property_images[0]?.image_path}
+                                title={property?.title}
+                                price={property?.price}
+                                rooms={property?.rooms}
+                                area={property?.area}
+                                floor={property?.floor}
+                                city={property?.city}
+                                address={property?.address}
+                                description={property?.description}
+                            />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
