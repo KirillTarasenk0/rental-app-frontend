8ae7be26f3e52375e3afc2369c521f2da3de482c
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-16 00:58:26 +0300

    Commercial component was made

--- src/pages/commercial/Commercial.jsx
+++ src/pages/commercial/Commercial.jsx
@@ -1,9 +1,32 @@
 import './Commercial.scss';
+import {useGetCommercialPropertiesQuery} from "../../slices/propertiesTypeApi";
+import {Link} from "react-router-dom";
+import {PropertyCard} from "../../components/propertyCard/PropertyCard";
 
 export const Commercial = () => {
+    const { data: properties, error, isLoading } = useGetCommercialPropertiesQuery();
     return (
-      <>
-        <h1>Commercial</h1>
-      </>
+        <>
+            <div className="all-properties">
+                {isLoading && <div className="all-properties__loading">Loading...</div>}
+                <div className="all-properties__list">
+                    {properties && properties.data?.map(property => (
+                        <Link to={`/property/${property.id}`} className="all-properties__link" key={property?.id}>
+                            <PropertyCard
+                                image={property?.property_images[0]?.image_path}
+                                title={property?.title}
+                                price={property?.price}
+                                rooms={property?.rooms}
+                                area={property?.area}
+                                floor={property?.floor}
+                                city={property?.city}
+                                address={property?.address}
+                                description={property?.description}
+                            />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
