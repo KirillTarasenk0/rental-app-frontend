2755a9625895ebb3a270d1a725347c975194053d
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-15 21:56:33 +0300

    PropertyCard was made

---
+++ src/components/propertyCard/PropertyCard.jsx
@@ -0,0 +1,27 @@
+import './PropertyCard.scss';
+
+export const PropertyCard = ({image, title, price, rooms, area, floor, city, address, description}) => {
+    return (
+      <>
+        <div>
+            <div>
+                {image ? <img src="" alt=""/> : <span>Изображение не зашружено</span>}
+            </div>
+            <div>
+                <h3>{title}</h3>
+                <p>{price}</p>
+                <div>
+                    <span>{rooms}</span>
+                    <span>{area}</span>
+                    <span>{floor}</span>
+                </div>
+                <div>
+                    <span>{city}</span>
+                    <span>{address}</span>
+                </div>
+                <p>{description}</p>
+            </div>
+        </div>
+      </>
+    );
+}
--- src/pages/allProperties/AllProperties.jsx
+++ src/pages/allProperties/AllProperties.jsx
@@ -1,10 +1,27 @@
 import './AllProperties.scss';
+import {useGetAllPropertiesQuery} from "../../slices/propertiesTypeApi";
+import {PropertyCard} from "../../components/propertyCard/PropertyCard";
 
 export const AllProperties = () => {
+    const {data: properties, error, isLoading} = useGetAllPropertiesQuery();
+    console.log(properties);
     return (
       <>
         <div>
-
+            {properties && properties.data?.map(property => (
+                <PropertyCard
+                    key={property?.id}
+                    image={property?.property_images}
+                    title={property?.title}
+                    price={property?.price}
+                    rooms={property?.rooms}
+                    area={property?.area}
+                    floor={property?.floor}
+                    city={property?.city}
+                    address={property?.address}
+                    description={property?.description}
+                />
+            ))}
         </div>
       </>
     );
